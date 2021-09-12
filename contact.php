e486e1bec7d9ea94d2fb43dba44f8a7fe5ffc61f
Auxxme <90456467+Auxxme@users.noreply.github.com>
2021-09-12 05:22:17 -0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Contact Us | Indika Phone Shop</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<!--start main tag-->
+<div class="main">
+
+<header>
+
+<div class="logo">
+<img src="image/logo.jpg" alt="indika phone shop" width="200px" height="100px" />
+</div>
+
+<div class="top-title">
+<h1>Indika Phone Shop</h1>
+</div>
+
+<div class="fallow">
+<img src="image/fb.png" alt="indika phone shop" width="50px" height="50px">
+<img src="image/g.png" alt="indika phone shop" width="50px" height="50px">
+<img src="image/skype.png" alt="indika phone shop" width="50px" height="50px">
+<img src="image/tw.png" alt="indika phone shop" width="40px" height="40px">
+<img src="image/yt.png" alt="indika phone shop" width="40px" height="40px">
+<img src="image/instr.png" alt="indika phone shop" width="40px" height="40px">
+</div>
+
+</header>
+
+
+
+<nav>
+
+<ul>
+<li><a href="index.html">Home</a></li>
+<li><a href="about.html">About</a></li>
+<li><a href="contact.html">Contact Us</a></li>
+<li><a href="mobile.html">Mobile</a></li>
+</ul>
+
+
+</nav>
+
+
+
+<div class="banner">
+<h2> Contact Us </h2>
+</div>
+
+<div class="contact-details">
+
+<div class="c-details">
+<h3>Mobile : 077 500 6065</h3>
+<h4>HotLine:041-5647895</h4>
+<h3>Address:No:87/B,Anagarika Dharmapala Mawatha,</h3>
+<h4>Email:Indikaphoneshop@gamil.com</h4>
+</div>
+
+<div class="c-details">
+<img src="image/pic7.jpg" alt="indika phone shop" width="480px" height="400px">
+</div>
+
+</div>
+
+<div class="map">
+<img src="image/map.png" alt="indika phone shop" width="1000px" height="500px">
+</div>
+
+
+
+<footer>
+
+<div class="contact">
+<h2>Contact Us</h2>
+<p>0425678952</p>
+<p>+94114348778</p>
+<p>indika@gmail.com</p>
+</div>
+
+<div class="contact">
+<h2>Quick Menue</h2>
+<p>Home</p>
+<p>About</p>
+<p>Contact</p>
+</div> 
+
+<div class="contact">
+<h2>Follow Us</h2>
+<p>Facebook</p>
+<p>Twitter</p>
+<p>Google</p> 
+</div>
+
+</footer>
+
+
+<div class="footer-end"><p>Coppy right By:T.M.Vithanage</p></div>
+
+</div>
+
+<!--end main tag-->
+
+
+</body>
+</html>
