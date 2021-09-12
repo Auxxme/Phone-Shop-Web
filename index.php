99e1559692c2ce21dd1c3124ae00e185e9fd6819
Auxxme <90456467+Auxxme@users.noreply.github.com>
2021-09-12 05:19:48 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Indika Phone Shop</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<!--start main tag-->
+<div class="main">
+
+<header>
+
+<div class="logo">
+<img src="image/logo.jpg" alt="indika phone shop" width="200px" height="100px" />
+</div>
+
+<div class="top-title">
+<h1>Indika Phone Shop</h1>
+</div>
+
+<div class="fallow">
+<img src="image/fb.png" alt="indika phone shop" width="50px" height="50px">
+<img src="image/g.png" alt="indika phone shop" width="50px" height="50px">
+<img src="image/skype.png" alt="indika phone shop" width="50px" height="50px">
+<img src="image/tw.png" alt="indika phone shop" width="40px" height="40px">
+<img src="image/yt.png" alt="indika phone shop" width="40px" height="40px">
+<img src="image/instr.png" alt="indika phone shop" width="40px" height="40px">
+</div>
+
+</header>
+
+
+
+<nav>
+
+<ul>
+<li><a href="index.html">Home</a></li>
+<li><a href="about.html">About</a></li>
+<li><a href="contact.html">Contact Us</a></li>
+<li><a href="mobile.html">Mobile</a></li>
+</ul>
+
+</nav>
+
+
+<div class="slider">
+<img src="image/baner1.png" alt="indika phone shop" width="1000px" height="500px">
+</div>
+
+
+<div class="forword">
+
+<div class="phone"><img src="image/aple.jpg" alt="indika phone shop" width="300px" height="307px"><b>Apple 8</b></div>
+<div class="phone"><img src="image/aple2.jpg" alt="phone" width="300px" height="308px"></div>
+<div class="phone"><img src="image/pic8.png" alt="phone" width="300px" height="307px"></div>
+<div class="phone"><img src="image/glxy.png" alt="indika phone shop" width="300px" height="307px"></div>
+<div class="phone"><img src="image/img21.png" alt="phone" width="300px" height="308px"></div>
+<div class="phone"><img src="image/img25.png" alt="phone" width="300px" height="307px"></div>
+
+
+</div>
+
+
+<footer>
+
+<div class="contact">
+<h2>Contact Us</h2>
+
+<p>0425678952</p>
+<p>+94114348778</p>
+<p>indika@gmail.com</p>
+</div>
+
+<div class="contact">
+<h2>Quick Menue</h2>
+<p>Home</p>
+<p>About</p>
+<p>Contact</p>
+</div> 
+
+<div class="contact">
+<h2>Follow Us</h2>
+<p>Facebook</p>
+<p>Twitter</p>
+<p>Google</p> 
+
+</div>
+
+</footer>
+
+
+<div class="footer-end"><p>Coppy right By:T.M.Vithanage</p></div>
+
+</div>
+<!--end main tag-->
+
+</body>
+</html>
