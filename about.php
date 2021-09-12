76c0ffa49f0ad502e9bb63b0c6dcc45a8d5ff751
Auxxme <90456467+Auxxme@users.noreply.github.com>
2021-09-12 05:21:41 -0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>About Us | Indika Phone Shop</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div class="main">
+
+<header>
+
+<div class="logo">
+<img src="image/logo.jpg" alt="indika phone shop" width="200px" height="100px" />
+</div>
+
+<div class="top-title">
+<h1>Indika Phone Shop</h1>
+</div>
+
+<div class="fallow">
+<img src="image/fb.png" alt="indika phone shop" width="50px" height="50px">
+<img src="image/g.png" alt="indika phone shop" width="50px" height="50px">
+<img src="image/skype.png" alt="indika phone shop" width="50px" height="50px">
+<img src="image/tw.png" alt="indika phone shop" width="40px" height="40px">
+<img src="image/yt.png" alt="indika phone shop" width="40px" height="40px">
+<img src="image/instr.png" alt="indika phone shop" width="40px" height="40px">
+
+</div>
+
+</header>
+
+
+
+<nav>
+
+<ul>
+
+<li><a href="index.php">Home</a></li>
+<li><a href="about.php">About</a></li>
+<li><a href="contact.php">Contact Us</a></li>
+<li><a href="mobile.php">Mobile</a></li>
+</ul>
+
+</nav>
+
+
+<div class="banner">
+<h2> About Us </h2>
+</div>
+
+<div class="welcome">
+<div class="welcome-image">
+<img src="image/pic2.png" alt="indika phone shop" width="380px" height="380px">
+</div>
+
+<div class="welcome-text">
+<p><b>HELO THERE</b></p>
+
+
+<p>We are glad you want to get to know us better.Now that you are in our list,you'll always be first to hear about everthing.<b>(HOW FANTASTIC)</b>.From emails about our<b> LATEST DESIGN </b> to not-to-be-missed SALE NEWS.We have you covered with the latest in cell phone covers & accessories and the gretest in ofiicially -licensed sports team gear.We also have a wide selection of gaming card accessories.shop INDIKA for great value,lightning fast FREE shipping and unrivaled customer service.
+</p>
+</div>
+</div>
+
+
+<div class="vision">
+
+<div class="vision1">
+
+<img src="image/visn.png" alt="indika phone shop" width="420" height="400px">
+<h2>VISION</h2>
+<p>vision is to be the world's mobile communication leader - enriching customers' lives, helping individuals, businesses and communities be more connected in a mobile world.</p>
+
+</div>
+
+<div class="vision1">
+<img src="image/mission.png" alt="indika phone shop" width="480px" height="400px">
+<h2>Mission</h2>
+<p>Vodafone is primarily a user of technology rather than a developer of it,and this fact is reflected in the emphasis of our work programme on enabling new applications of mobile communications,using new technology for new services,research for improving operational efficiency and quality of oue networks,and providing technology vission and leardership that can contribute directly to business decissions.</p>
+</div>
+
+</div>
+
+<footer>
+
+<div class="contact">
+<h2>Contact Us</h2>
+
+<p>0425678952</p>
+<p>+94114348778</p>
+<p>indika@gmail.com</p>
+</div>
+
+<div class="contact">
+<h2>Quick Menue</h2>
+<p>Home</p>
+<p>About</p>
+<p>Contact</p>
+</div> 
+
+<div class="contact">
+<h2>Follow Us</h2>
+<p>Facebook</p>
+<p>Twitter</p>
+<p>Google</p> 
+
+</div>
+
+</footer>
+
+
+<div class="footer-end"><p>Coppy right By:T.M.Vithanage</p></div>
+
+</div>
+<!--end main tag-->
+
+</body>
+</html>
